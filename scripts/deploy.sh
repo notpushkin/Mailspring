9d33bea3d182064c754e51a8834c49cd0334c04b
Nate Ijams <nate@ijams.me>
2020-12-16 18:51:12 -0600

    Update deployment to post builds on new server.
    Now uploads builds to files.mailspringlibre.org.
    Catbox.moe is too finnicky and unreliable (half the time uploads will
    fail).

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 shopt -s nullglob
 cd app/dist/
 ls
-for f in *.{dmg,zip,deb,rpm,msi}; do
-  curl -F "fileToUpload=@$f" -F 'reqtype=fileupload' 'https://catbox.moe/user/api.php'
+for f in *.{dmg,zip,deb,rpm,msi,exe}; do
+  curl -F "file=@$f" https://files.mailspringlibre.org
   printf "\n"
 done
